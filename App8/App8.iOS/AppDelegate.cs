02cd64b1e6e5139e84e385c56117f54268ff3818
Pawel Pabich <pawel@pabich.eu>
2015-04-28 17:09:17 +1000

    Sample code for contacts

---
+++ App8/App8.iOS/AppDelegate.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AddressBook;
+using AddressBookUI;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace App8.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Global.Doer = new iOSDoer();
+            Forms.Init();
+            LoadApplication(new App());
+            
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+
+    public class iOSDoer : IDo
+    {
+        public void ShowContact(NavigationPage page)
+        {
+            var newPersonController = new ABNewPersonViewController();
+            var person = new ABPerson();
+            person.FirstName = "John";
+            person.LastName = "Doe";
+
+
+            newPersonController.DisplayedPerson = person;
+            var controller = page.CreateViewController();
+
+            //controller.NavigationController is null
+            controller.NavigationController.PushViewController(newPersonController, true);            
+        }
+
+        public IList<string> GetAllNames()
+        {
+            var people = new ABAddressBook().GetPeople();
+            return people.Select(p => p.FirstName).ToList();
+        }
+    }
+}
---
+++ App8/App8/App.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App8
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var page = new ContentPage
+            {
+            };
+
+            var navigationPage = new NavigationPage(page);
+            var command = new Command(AddContact);
+            var list = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    new Label {
+                        XAlign = TextAlignment.Center,
+                        Text = "Welcome to Xamarin Forms!"  + GetNames()
+                    },
+                    new Button()
+                    {
+                        Text = "Click me!",
+                        Command = command,
+                        CommandParameter = navigationPage
+                    }
+                }
+            };
+
+            page.Content = list;
+
+          
+            MainPage = navigationPage;
+     
+        }
+
+        private string GetNames()
+        {
+            var buffer = new StringBuilder();
+            foreach (var name in Global.Doer.GetAllNames())
+            {
+                buffer.AppendLine(name);
+            }
+
+            return buffer.ToString();
+        }
+
+        private void AddContact(object obj)
+        {
+            Global.Doer.ShowContact((NavigationPage)obj);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+
+    public static class Global
+    {
+        public static IDo Doer { get; set; }
+    }
+
+    public interface IDo
+    {
+        void ShowContact(NavigationPage page);
+        IList<string> GetAllNames();
+    }
+}
